17fe008606a7957fe33b07e9cbf93fde8c4d356b
Santiago Dueñas <sduenas@bitergia.com>
2014-12-19 12:00:10 +0100

    [matching] Add SimpleMatcher class
    This matcher only produces a positive result when two identities
    from each unique identity share the same email address. It also
    returns a positive match when the uuid on both unique identities
    is equal.
    
    Unit tests also added.

---
+++ sortinghat/matching/simple.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+from sortinghat.db.model import UniqueIdentity
+from sortinghat.matcher import IdentityMatcher
+
+
+class SimpleMatcher(IdentityMatcher):
+    """
+    Simple unique identities matcher.
+
+    This matcher only produces a positive result when two identities
+    from each unique identity share the same email address. It also
+    returns a positive match when the uuid on both unique identities is equal.
+    """
+    def __init__(self):
+        super(SimpleMatcher, self).__init__()
+
+    def match(self, a, b):
+        """Determine if two unique identities are the same.
+
+        This method compares the email addresses of each identity to check
+        if the given unique identities are the same. When the given unique
+        identities are the same object or share the same uuid, this will
+        also produce a positive match.
+
+        :param a: unique identity to match
+        :param b: unique identity to match
+
+        :returns: True when both unique identities are likely to be the same.
+            Otherwise, returns False.
+
+        :raises ValueError: when any of the given unique identities is not
+            an instance of UniqueIdentity class
+        """
+        if not isinstance(a, UniqueIdentity):
+            raise ValueError('<a> is not an instance of UniqueIdentity')
+        if not isinstance(b, UniqueIdentity):
+            raise ValueError('<b> is not an instance of UniqueIdentity')
+
+        if a.uuid == b.uuid:
+            return True
+
+        emails_a = self._filter_emails(a.identities)
+        emails_b = self._filter_emails(b.identities)
+
+        for email in emails_a:
+            if email in emails_b:
+                return True
+        return False
+
+    def _filter_emails(self, ids):
+        return [id.email.lower() for id in ids if id.email]
---
+++ tests/test_matcher_simple.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+import sys
+import unittest
+
+if not '..' in sys.path:
+    sys.path.insert(0, '..')
+
+from sortinghat import api
+from sortinghat.db.model import UniqueIdentity, Identity
+from sortinghat.matching.simple import SimpleMatcher
+
+
+class TestSimpleMatcher(unittest.TestCase):
+
+    def test_match(self):
+        """Test match method"""
+
+        # Let's define some identities first
+        jsmith = UniqueIdentity(uuid='jsmith')
+        jsmith.identities = [Identity(name='John Smith', email='jsmith@example.com', source='scm'),
+                             Identity(name='John Smith', source='scm'),
+                             Identity(username='jsmith', source='scm'),
+                             Identity(email='', source='scm')]
+
+        john_smith = UniqueIdentity(uuid='jsmith')
+        john_smith.identities = [Identity(name='J. Smith', username='john_smith', source='scm'),
+                                 Identity(username='john_smith', source='scm'),
+                                 Identity(name='Smith. J', source='mls'),
+                                 Identity(name='Smith. J', email='JSmith@example.com', source='mls')]
+
+        jsmith_alt = UniqueIdentity(uuid='J. Smith')
+        jsmith_alt.identities = [Identity(name='J. Smith', username='john_smith', source='alt'),
+                                 Identity(name='John Smith', username='jsmith', source='alt'),
+                                 Identity(email='', source='alt')]
+
+        # Tests
+        matcher = SimpleMatcher()
+
+        # First two unique identities must match
+        result = matcher.match(jsmith, john_smith)
+        self.assertEqual(result, True)
+
+        result = matcher.match(john_smith, jsmith)
+        self.assertEqual(result, True)
+
+        # Comparing with the third does not produce any match
+        result = matcher.match(jsmith, jsmith_alt)
+        self.assertEqual(result, False)
+
+        result = matcher.match(jsmith_alt, jsmith)
+        self.assertEqual(result, False)
+
+        result = matcher.match(john_smith, jsmith_alt)
+        self.assertEqual(result, False)
+
+        result = matcher.match(jsmith_alt, john_smith)
+        self.assertEqual(result, False)
+
+
+    def test_match_same_identity(self):
+        """Test whether there is a match comparing the same identity"""
+
+        uid = UniqueIdentity(uuid='John Smith')
+
+        matcher = SimpleMatcher()
+        result = matcher.match(uid, uid)
+
+        self.assertEqual(result, True)
+
+    def test_identities_instances(self):
+        """Test whether it raises an error when ids are not UniqueIdentities"""
+
+        uid = UniqueIdentity(uuid='John Smith')
+
+        matcher = SimpleMatcher()
+
+        self.assertRaises(ValueError, matcher.match, 'John Smith', uid)
+        self.assertRaises(ValueError, matcher.match, uid, 'John Smith')
+        self.assertRaises(ValueError, matcher.match, None, uid)
+        self.assertRaises(ValueError, matcher.match, uid, None)
+        self.assertRaises(ValueError, matcher.match, 'John Smith', 'John Doe')
+
+
+if __name__ == "__main__":
+    unittest.main()
