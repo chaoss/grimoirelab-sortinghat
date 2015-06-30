60c8202f101d6c90a24778b7f93b97d13e9c1a6f
Santiago Dueñas <sduenas@bitergia.com>
2015-06-30 19:52:58 +0200

    [parsing:eclipse] Add Eclipse parser

---
+++ sortinghat/parsing/eclipse.py
@@ -0,0 +1,269 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2015 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+from sortinghat.db.model import MIN_PERIOD_DATE, MAX_PERIOD_DATE, \
+    UniqueIdentity, Identity, Enrollment, Organization
+from sortinghat.exceptions import InvalidDateError, InvalidFormatError
+from sortinghat.utils import str_to_datetime
+
+
+class EclipseParser(object):
+    """Parse identities and organizations using Eclipse format.
+
+    The Eclipse data format is a JSON stream that contains
+    information about unique identities and organizations.
+
+    The unique identities are stored in an object named 'uidentities'.
+    The keys of this object are the UUID of the unique identities.
+    Each unique identity object stores a list of identities and
+    enrollments.
+
+    Organizations are stored in 'organizations' object. Its keys
+    are the name of the organizations and each organization object is
+    related to a list of domains.
+
+    :param stream: stream to parse
+
+    :raises InvalidFormatError: raised when the format of the stream is
+        not valid.
+    """
+    def __init__(self, stream, source='eclipse'):
+        self._identities = {}
+        self._organizations = {}
+        self.source = source
+
+        self.__parse(stream)
+
+    @property
+    def identities(self):
+        uids = [u for u in self._identities.values()]
+        uids.sort(key=lambda u: u.uuid)
+        return uids
+
+    @property
+    def organizations(self):
+        orgs = [o for o in self._organizations.values()]
+        orgs.sort(key=lambda o: o.name)
+        return orgs
+
+    def __parse(self, stream):
+        """Parse Eclipse stream"""
+
+        if not stream:
+            raise InvalidFormatError(cause="stream cannot be empty or None")
+
+        json = self.__load_json(stream)
+
+        self.__parse_organizations(json)
+        self.__parse_identities(json)
+
+    def __parse_identities(self, json):
+        """Parse identities using Eclipse format.
+
+        The Eclipse identities format is a JSON document under the "commiters"
+        key. The document should follow the next schema:
+
+        {
+          'committers' : {
+              'john': {
+                  'affiliations': {
+                      '1': {
+                          'active': '2001-01-01',
+                          'inactive': null,
+                          'name': 'Organization 1'
+                      }
+                  },
+                  'email': [
+                      'john@example.com'
+                  ],
+                  'first': 'John',
+                  'id': 'john',
+                  'last': 'Doe',
+                  'primary': 'john.doe@example.com'
+              }
+          }
+        }
+
+        :parse json: JSON object to parse
+
+        :raise InvalidFormatError: raised when the format of the JSON is
+            not valid.
+        """
+        try:
+            for committer in json['committers'].values():
+                name = self.__encode(committer['first'] + ' ' + committer['last'])
+                email = self.__encode(committer['primary'])
+                username = self.__encode(committer['id'])
+                uuid = username
+
+                uid = UniqueIdentity(uuid=uuid)
+                identity = Identity(name=name, email=email, username=username,
+                                    source=self.source, uuid=uuid)
+                uid.identities.append(identity)
+
+                if 'email' in committer:
+                    for alt_email in committer['email']:
+                        alt_email = self.__encode(alt_email)
+
+                        if alt_email == email:
+                            continue
+
+                        identity = Identity(name=name, email=alt_email, username=username,
+                                            source=self.source, uuid=uuid)
+                        uid.identities.append(identity)
+
+                if 'affiliations' in committer:
+                    enrollments = self.__parse_affiliations_json(committer['affiliations'],
+                                                                 uuid)
+                    for rol in enrollments:
+                        uid.enrollments.append(rol)
+
+                self._identities[uuid] = uid
+        except KeyError, e:
+            msg = "invalid json format. Attribute %s not found" % e.args
+            raise InvalidFormatError(cause=msg)
+
+    def __parse_organizations(self, json):
+        """Parse Eclipse organizations.
+
+        The Eclipse organizations format is a JSON document stored under the
+        "organizations" key. The next JSON shows the structure of the
+        document:
+
+        {
+          'organizations' : {
+               '1': {
+                   'active': '2001-01-01 18:00:00',
+                   'id': '1',
+                   'image' : 'http://example.com/image/1',
+                   'inactive' : null,
+                   'isMember': '1',
+                   'name': 'Organization 1',
+                   'url': 'http://example.com/org/1'
+                    },
+               '2': {
+                   'active': '2015-12-31 23:59:59',
+                   'id': '1',
+                   'image' : 'http://example.com/image/2',
+                   'inactive': '2008-01-01 18:00:00',
+                   'isMember': '1',
+                   'name': 'Organization 2',
+                   'url': 'http://example.com/org/2'
+                    }
+          }
+        }
+
+        :param json: JSON object to parse
+
+        :raises InvalidFormatError: raised when the format of the JSON is
+            not valid.
+        """
+        try:
+            for organization in json['organizations'].values():
+                name = self.__encode(organization['name'])
+
+                try:
+                    active = str_to_datetime(organization['active'])
+                    inactive = str_to_datetime(organization['inactive'])
+
+                    # Ignore organization
+                    if not active and not inactive:
+                        continue
+
+                    if not active:
+                        active = MIN_PERIOD_DATE
+                    if not inactive:
+                        inactive = MAX_PERIOD_DATE
+                except InvalidDateError, e:
+                    raise InvalidFormatError(cause=str(e))
+
+                org = self._organizations.get(name, None)
+
+                if not org:
+                    org = Organization(name=name)
+
+                    # Store metadata valid for identities parsing
+                    org.active = active
+                    org.inactive = inactive
+
+                    self._organizations[name] = org
+        except KeyError, e:
+            msg = "invalid json format. Attribute %s not found" % e.args
+            raise InvalidFormatError(cause=msg)
+
+    def __parse_affiliations_json(self, affiliations, uuid):
+        """Parse identity's affiliations from a json dict"""
+
+        enrollments = []
+
+        for affiliation in affiliations.values():
+            name = affiliation['name'].encode('UTF-8')
+
+            try:
+                start_date = str_to_datetime(affiliation['active'])
+                end_date = str_to_datetime(affiliation['inactive'])
+            except InvalidDateError, e:
+                raise InvalidFormatError(cause=str(e))
+
+            # Ignore affiliation
+            if not start_date and not end_date:
+                continue
+
+            if not start_date:
+                start_date = MIN_PERIOD_DATE
+            if not end_date:
+                end_date = MAX_PERIOD_DATE
+
+            org = self._organizations.get(name, None)
+
+            # Set enrolllment period according to organization data
+            if org:
+                start_date = org.active if start_date < org.active else start_date
+                end_date = org.inactive if end_date > org.inactive else end_date
+
+            if not org:
+                org = Organization(name=name)
+                org.active = MIN_PERIOD_DATE
+                org.inactive = MAX_PERIOD_DATE
+
+            enrollment = Enrollment(start=start_date, end=end_date,
+                                    organization=org)
+            enrollments.append(enrollment)
+
+        return enrollments
+
+    def __load_json(self, stream):
+        """Load json stream into a dict object """
+
+        import json
+
+        try:
+            return json.loads(stream)
+        except ValueError, e:
+            cause = "invalid json format. %s" % str(e)
+            raise InvalidFormatError(cause=cause)
+
+    def __encode(self, s):
+        if type(s) is str:
+            return s.encode('UTF-8') if s else None
+        else:
+            return s
---
+++ tests/test_parser_eclipse.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2015 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+import datetime
+import sys
+import unittest
+
+if not '..' in sys.path:
+    sys.path.insert(0, '..')
+
+from sortinghat.db.model import UniqueIdentity, Identity, Organization
+from sortinghat.exceptions import InvalidFormatError
+from sortinghat.parsing.eclipse import EclipseParser
+
+
+ECLIPSE_INVALID_JSON_FORMAT_ERROR = r"invalid json format\. Expecting ':' delimiter"
+ECLIPSE_IDS_MISSING_KEYS_ERROR = "Attribute active not found"
+ECLIPSE_ORGS_MISSING_KEYS_ERROR = "Attribute name not found"
+ECLIPSE_DATETIME_ERROR = "2008-02-30 is not a valid date"
+ECLIPSE_STREAM_INVALID_ERROR = "stream cannot be empty or None"
+
+
+class TestBaseCase(unittest.TestCase):
+    """Defines common methods for unit tests"""
+
+    def read_file(self, filename):
+        with open(filename, 'r') as f:
+            content = f.read().decode('UTF-8')
+        return content
+
+
+class TestEclipseParser(TestBaseCase):
+    """Test Eclipse parser with some inputs"""
+
+    def test_valid_identities_stream(self):
+        """Check insertion of valid data from a file"""
+
+        stream = self.read_file('data/eclipse_valid.json')
+
+        parser = EclipseParser(stream, source='unknown')
+        uids = parser.identities
+
+        # Check parsed identities
+        self.assertEqual(len(uids), 3)
+
+        # John Doe unique identity
+        uid = uids[0]
+        self.assertIsInstance(uid, UniqueIdentity)
+        self.assertEqual(uid.uuid, 'jdoe')
+        self.assertEqual(len(uid.identities), 1)
+
+        id0 = uid.identities[0]
+        self.assertIsInstance(id0, Identity)
+        self.assertEqual(id0.id, None)
+        self.assertEqual(id0.name, 'John Doe')
+        self.assertEqual(id0.email, 'jdoe@example.com')
+        self.assertEqual(id0.username, 'jdoe')
+        self.assertEqual(id0.uuid, 'jdoe')
+        self.assertEqual(id0.source, 'unknown')
+
+        enrollments = uid.enrollments
+        self.assertEqual(len(enrollments), 1)
+
+        rol0 = enrollments[0]
+        self.assertIsInstance(rol0.organization, Organization)
+        self.assertEqual(rol0.organization.name, 'Example')
+        self.assertEqual(rol0.start, datetime.datetime(2010, 1, 1, 0, 0, 0))
+
+        # The parsed end date shoud be 2100-01-01 but because there is
+        # an inactive date for Example, this end date is set to the
+        # inactive date
+        self.assertEqual(rol0.end, datetime.datetime(2014, 9, 1, 0, 0, 0))
+
+        # Jane Rae identity
+        uid = uids[1]
+        self.assertIsInstance(uid, UniqueIdentity)
+        self.assertEqual(uid.uuid, 'jrae')
+        self.assertEqual(len(uid.identities), 1)
+
+        id0 = uid.identities[0]
+        self.assertIsInstance(id0, Identity)
+        self.assertEqual(id0.id, None)
+        self.assertEqual(id0.name, 'Jane Rae')
+        self.assertEqual(id0.email, 'jrae@example.com')
+        self.assertEqual(id0.username, 'jrae')
+        self.assertEqual(id0.uuid, 'jrae')
+        self.assertEqual(id0.source, 'unknown')
+
+        enrollments = uid.enrollments
+        self.assertEqual(len(enrollments), 0)
+
+        # John Smith unique identity
+        uid = uids[2]
+        self.assertIsInstance(uid, UniqueIdentity)
+        self.assertEqual(uid.uuid, 'jsmith')
+        self.assertEqual(len(uid.identities), 2)
+
+        id0 = uid.identities[0]
+        self.assertIsInstance(id0, Identity)
+        self.assertEqual(id0.id, None)
+        self.assertEqual(id0.name, 'John Smith')
+        self.assertEqual(id0.email, 'jsmith@example.com')
+        self.assertEqual(id0.username, 'jsmith')
+        self.assertEqual(id0.uuid, 'jsmith')
+        self.assertEqual(id0.source, 'unknown')
+
+        id1 = uid.identities[1]
+        self.assertIsInstance(id1, Identity)
+        self.assertEqual(id1.id, None)
+        self.assertEqual(id1.name, 'John Smith')
+        self.assertEqual(id1.email, 'jsmith@bitergia.com')
+        self.assertEqual(id1.username, 'jsmith')
+        self.assertEqual(id1.uuid, 'jsmith')
+        self.assertEqual(id1.source, 'unknown')
+
+        enrollments = uid.enrollments
+        self.assertEqual(len(enrollments), 2)
+
+        rol0 = enrollments[0]
+        self.assertIsInstance(rol0.organization, Organization)
+        self.assertEqual(rol0.organization.name, 'Example')
+        self.assertEqual(rol0.start, datetime.datetime(2010, 1, 1))
+        self.assertEqual(rol0.end, datetime.datetime(2011, 1, 1))
+
+        rol1 = enrollments[1]
+        self.assertIsInstance(rol1.organization, Organization)
+        self.assertEqual(rol1.organization.name, 'Bitergia')
+
+        # Active date for Bitergia is 2012-01-01, so this enrollment
+        # has that date instead of 2011-01-01
+        self.assertEqual(rol1.start, datetime.datetime(2012, 1, 1))
+        self.assertEqual(rol1.end, datetime.datetime(2100, 1, 1))
+
+    def test_valid_organizations_stream(self):
+        """Check whether it parses organizations section from a valid stream"""
+
+        stream = self.read_file('data/eclipse_valid.json')
+
+        parser = EclipseParser(stream)
+        orgs = parser.organizations
+
+        # Check parsed organizations
+        self.assertEqual(len(orgs), 2)
+
+        # This objects store additional information
+        # regarding when an organization is active or inactive
+
+        # Bitergia entry
+        org = orgs[0]
+        self.assertIsInstance(org, Organization)
+        self.assertEqual(org.name, 'Bitergia')
+        self.assertEqual(org.active, datetime.datetime(2012, 1, 1, 0, 0, 0))
+        self.assertEqual(org.inactive, datetime.datetime(2100, 1, 1, 0, 0, 0))
+
+        # Example entry
+        org = orgs[1]
+        self.assertIsInstance(org, Organization)
+        self.assertEqual(org.name, 'Example')
+        self.assertEqual(org.active, datetime.datetime(2009, 1, 1, 0, 0, 0))
+        self.assertEqual(org.inactive, datetime.datetime(2014, 9, 1, 0, 0, 0))
+
+    def test_not_valid_stream(self):
+        """Check whether it prints an error when parsing invalid streams"""
+
+        with self.assertRaisesRegexp(InvalidFormatError,
+                                     ECLIPSE_INVALID_JSON_FORMAT_ERROR):
+            s = self.read_file('data/eclipse_invalid.json')
+            EclipseParser(s)
+
+        with self.assertRaisesRegexp(InvalidFormatError,
+                                     ECLIPSE_IDS_MISSING_KEYS_ERROR):
+            s = self.read_file('data/eclipse_ids_missing_keys.json')
+            EclipseParser(s)
+
+        with self.assertRaisesRegexp(InvalidFormatError,
+                                     ECLIPSE_ORGS_MISSING_KEYS_ERROR):
+            s = self.read_file('data/eclipse_orgs_missing_keys.json')
+            EclipseParser(s)
+
+        with self.assertRaisesRegexp(InvalidFormatError,
+                                     ECLIPSE_DATETIME_ERROR):
+            s = self.read_file('data/eclipse_invalid_datetime.json')
+            EclipseParser(s)
+
+    def test_empty_stream(self):
+        """Check whether it raises an exception when the stream is empty"""
+
+        with self.assertRaisesRegexp(InvalidFormatError,
+                                     ECLIPSE_STREAM_INVALID_ERROR):
+            EclipseParser("")
+
+    def test_none_stream(self):
+        """Check whether it raises an exception when the stream is None"""
+
+        with self.assertRaisesRegexp(InvalidFormatError,
+                                     ECLIPSE_STREAM_INVALID_ERROR):
+            EclipseParser(None)
+
+
+if __name__ == "__main__":
+    unittest.main(buffer=True, exit=False)
