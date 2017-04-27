0505cba0521e2b09c5b2f9a19c5e7ad970c874c0
Santiago Dueñas <sduenas@bitergia.com>
2017-04-27 13:20:29 +0200

    [parsing:stackalytics] Add Stackalytics parser

---
+++ sortinghat/parsing/stackalytics.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2017 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#	 Santiago Dueñas <sduenas@bitergia.com>
+#
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from ..db.model import MIN_PERIOD_DATE, MAX_PERIOD_DATE, \
+    UniqueIdentity, Identity, Enrollment, Organization, Domain
+from ..exceptions import InvalidDateError, InvalidFormatError
+from ..utils import str_to_datetime
+
+
+class StackalyticsParser(object):
+    """Parse identities and organizations using the Stackalytics format.
+
+    The Stackalytics data format is a JSON stream that contains
+    information about unique identities and organizations.
+
+    The unique identities are stored in an object named 'users'.
+    Each 'user' is an object that stores its email addresses
+    and enrollments or affiliations.
+
+    Organizations are stored in the 'companies' object. Each object
+    stores the name of the organization, its aliases and domains.
+
+    :param stream: stream to parse
+
+    :raises InvalidFormatError: raised when the format of the stream is
+    not valid.
+    """
+    def __init__(self, stream, source='stackalytics'):
+        self._identities = {}
+        self._organizations = {}
+        self.source = source
+
+        self.__parse(stream)
+
+    def __parse(self, stream):
+        """Parse Stackalytics stream"""
+
+        if not stream:
+            raise InvalidFormatError(cause="stream cannot be empty or None")
+
+        json = self.__load_json(stream)
+
+        self.__parse_organizations(json)
+        self.__parse_identities(json)
+
+    @property
+    def identities(self):
+        uids = [u for u in self._identities.values()]
+        uids.sort(key=lambda u: u.uuid)
+        return uids
+
+    @property
+    def organizations(self):
+        orgs = [o for o in self._organizations.values()]
+        orgs.sort(key=lambda o: o.name)
+        return orgs
+
+    def __parse_organizations(self, json):
+        """Parse Stackalytics organizations.
+
+        The Stackalytics organizations format is a JSON document stored under the
+        "companies" key. The next JSON shows the structure of the
+        document:
+
+        {
+            "companies" : [
+                {
+                    "domains": ["alcatel-lucent.com"],
+                    "company_name": "Alcatel-Lucent",
+                    "aliases": ["Alcatel Lucent", "Alcatel-Lcuent"]
+                },
+                {
+                    "domains": ["allegrogroup.com", "allegro.pl"],
+                    "company_name": "Allegro",
+                    "aliases": ["Allegro Group", "Grupa Allegro", "Grupa Allegro Sp. z o.o."]
+                },
+                {
+                    "domains": ["altiscale.com"],
+                    "company_name": "Altiscale"
+                },
+            ]
+        }
+
+        :param json: JSON object to parse
+
+        :raises InvalidFormatError: raised when the format of the JSON is
+            not valid.
+        """
+        try:
+            for company in json['companies']:
+                name = self.__encode(company['company_name'])
+
+                org = self._organizations.get(name, None)
+
+                if not org:
+                    org = Organization(name=name)
+                    self._organizations[name] = org
+
+                for domain in company['domains']:
+                    if not domain:
+                        continue
+                    dom = Domain(domain=domain)
+                    org.domains.append(dom)
+        except KeyError as e:
+            msg = "invalid json format. Attribute %s not found" % e.args
+            raise InvalidFormatError(cause=msg)
+
+    def __parse_identities(self, json):
+        """Parse identities using Stackalytics format.
+
+        The Stackalytics identities format is a JSON document under the
+        "users" key. The document should follow the next schema:
+
+        {
+            "users": [
+                {
+                    "launchpad_id": "0-jsmith",
+                    "gerrit_id": "jsmith",
+                    "companies": [
+                        {
+                            "company_name": "Example",
+                            "end_date": null
+                        }
+                    ],
+                    "user_name": "John Smith",
+                    "emails": ["jsmith@example.com", "jsmith@example.net"]
+                },
+                {
+                    "companies": [
+                        {
+                            "company_name": "Bitergia",
+                            "end_date": null
+                        },
+                        {
+                            "company_name": "Example",
+                            "end_date": "2010-Jan-01"
+                        }
+                    ],
+                    "user_name": "John Doe",
+                    "emails": ["jdoe@bitergia.com", "jdoe@example.com"]
+                }
+            ]
+        }
+
+        :parse json: JSON object to parse
+
+        :raise InvalidFormatError: raised when the format of the JSON is
+            not valid.
+        """
+        try:
+            for user in json['users']:
+                name = self.__encode(user['user_name'])
+                uuid = name
+
+                uid = UniqueIdentity(uuid=uuid)
+                identity = Identity(name=name, email=None, username=None,
+                                    source=self.source, uuid=uuid)
+                uid.identities.append(identity)
+
+                for email_addr in user['emails']:
+                    email = self.__encode(email_addr)
+
+                    identity = Identity(name=name, email=email, username=None,
+                                        source=self.source, uuid=uuid)
+                    uid.identities.append(identity)
+
+                for site_id in ['gerrit_id', 'launchpad_id']:
+                    username = user.get(site_id, None)
+
+                    if not username:
+                        continue
+
+                    username = self.__encode(username)
+                    source = self.source + ':' + site_id.replace('_id', '')
+                    identity = Identity(name=name, email=None, username=username,
+                                        source=source, uuid=uuid)
+                    uid.identities.append(identity)
+
+                for rol in self.__parse_enrollments(user):
+                    uid.enrollments.append(rol)
+
+                self._identities[uuid] = uid
+        except KeyError as e:
+            msg = "invalid json format. Attribute %s not found" % e.args
+            raise InvalidFormatError(cause=msg)
+
+    def __parse_enrollments(self, user):
+        """Parse user enrollments"""
+
+        enrollments = []
+
+        for company in user['companies']:
+            name = company['company_name']
+
+            org = self._organizations.get(name, None)
+
+            if not org:
+               org = Organization(name=name)
+               self._organizations[name] = org
+
+            start_date = MIN_PERIOD_DATE
+            end_date = MAX_PERIOD_DATE
+
+            if company['end_date']:
+                end_date = str_to_datetime(company['end_date'])
+
+            rol = Enrollment(start=start_date, end=end_date,
+                             organization=org)
+            enrollments.append(rol)
+
+        return enrollments
+
+    def __load_json(self, stream):
+        """Load json stream into a dict object """
+
+        import json
+
+        try:
+            return json.loads(stream)
+        except ValueError as e:
+            cause = "invalid json format. %s" % str(e)
+        raise InvalidFormatError(cause=cause)
+
+    def __encode(self, s):
+        import sys
+
+        if sys.version_info[0] >= 3: # Python 3
+            return s if s else None
+        else: # Python 2
+            if type(s) is str:
+                return s.encode('UTF-8') if s else None
+            else:
+                return s
---
+++ tests/test_parser_stackalytics.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2017 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+from __future__ import unicode_literals
+
+import datetime
+import sys
+import unittest
+
+if '..' not in sys.path:
+    sys.path.insert(0, '..')
+
+from sortinghat.db.model import MIN_PERIOD_DATE, MAX_PERIOD_DATE, \
+    UniqueIdentity, Identity, Enrollment, Organization, Domain
+from sortinghat.exceptions import InvalidFormatError
+from sortinghat.parsing.stackalytics import StackalyticsParser
+
+
+STACKALYTICS_INVALID_JSON_FORMAT_ERROR = r"invalid json format\. Expecting ':' delimiter"
+STACKALYTICS_IDS_MISSING_KEYS_ERROR = "Attribute companies not found"
+STACKALYTICS_ORGS_MISSING_KEYS_ERROR = "Attribute end_date not found"
+STACKALYTICS_STREAM_INVALID_ERROR = "stream cannot be empty or None"
+
+
+class TestBaseCase(unittest.TestCase):
+    """Defines common methods for unit tests"""
+
+    def read_file(self, filename):
+        if sys.version_info[0] >= 3: # Python 3
+            with open(filename, 'r', encoding='UTF-8') as f:
+                content = f.read()
+        else: # Python 2
+            with open(filename, 'r') as f:
+                content = f.read().decode('UTF-8')
+        return content
+
+
+class TestStackalyticsParser(TestBaseCase):
+    """Test Stackalytics parser with some inputs"""
+
+    def test_valid_identities_stream(self):
+        """Check insertion of valid data from a file"""
+
+        stream = self.read_file('data/stackalytics_valid.json')
+
+        parser = StackalyticsParser(stream, source='unknown')
+        uids = parser.identities
+
+        # Check parsed identities
+        self.assertEqual(len(uids), 2)
+
+        # John Doe unique identity
+        uid = uids[0]
+        self.assertIsInstance(uid, UniqueIdentity)
+        self.assertEqual(uid.uuid, 'John Doe')
+        self.assertEqual(len(uid.identities), 3)
+
+        id0 = uid.identities[0]
+        self.assertIsInstance(id0, Identity)
+        self.assertEqual(id0.id, None)
+        self.assertEqual(id0.name, 'John Doe')
+        self.assertEqual(id0.email, None)
+        self.assertEqual(id0.username, None)
+        self.assertEqual(id0.uuid, 'John Doe')
+        self.assertEqual(id0.source, 'unknown')
+
+        enrollments = uid.enrollments
+        self.assertEqual(len(enrollments), 2)
+
+        rol0 = enrollments[0]
+        self.assertIsInstance(rol0, Enrollment)
+        self.assertIsInstance(rol0.organization, Organization)
+        self.assertEqual(rol0.organization.name, 'Bitergia')
+        self.assertEqual(rol0.start, MIN_PERIOD_DATE)
+        self.assertEqual(rol0.end, MAX_PERIOD_DATE)
+
+        rol1 = enrollments[1]
+        self.assertIsInstance(rol1, Enrollment)
+        self.assertIsInstance(rol1.organization, Organization)
+        self.assertEqual(rol1.organization.name, 'Example')
+        self.assertEqual(rol1.start, MIN_PERIOD_DATE)
+        self.assertEqual(rol1.end, datetime.datetime(2010, 1, 1, 0, 0, 0))
+
+        # John Smith unique identity
+        uid = uids[1]
+        self.assertIsInstance(uid, UniqueIdentity)
+        self.assertEqual(uid.uuid, 'John Smith')
+        self.assertEqual(len(uid.identities), 4)
+
+        id0 = uid.identities[0]
+        self.assertIsInstance(id0, Identity)
+        self.assertEqual(id0.id, None)
+        self.assertEqual(id0.name, 'John Smith')
+        self.assertEqual(id0.email, None)
+        self.assertEqual(id0.username, None)
+        self.assertEqual(id0.uuid, 'John Smith')
+        self.assertEqual(id0.source, 'unknown')
+
+        id1 = uid.identities[1]
+        self.assertIsInstance(id1, Identity)
+        self.assertEqual(id1.id, None)
+        self.assertEqual(id1.name, 'John Smith')
+        self.assertEqual(id1.email, 'jsmith@example.com')
+        self.assertEqual(id1.username, None)
+        self.assertEqual(id1.uuid, 'John Smith')
+        self.assertEqual(id1.source, 'unknown')
+
+        id2 = uid.identities[2]
+        self.assertIsInstance(id2, Identity)
+        self.assertEqual(id2.id, None)
+        self.assertEqual(id2.name, 'John Smith')
+        self.assertEqual(id2.email, None)
+        self.assertEqual(id2.username, 'jsmith')
+        self.assertEqual(id2.uuid, 'John Smith')
+        self.assertEqual(id2.source, 'unknown:gerrit')
+
+        id3 = uid.identities[3]
+        self.assertIsInstance(id3, Identity)
+        self.assertEqual(id3.id, None)
+        self.assertEqual(id3.name, 'John Smith')
+        self.assertEqual(id3.email, None)
+        self.assertEqual(id3.username, '0-jsmith')
+        self.assertEqual(id3.uuid, 'John Smith')
+        self.assertEqual(id3.source, 'unknown:launchpad')
+
+        enrollments = uid.enrollments
+        self.assertEqual(len(enrollments), 1)
+
+        rol0 = enrollments[0]
+        self.assertIsInstance(rol0, Enrollment)
+        self.assertIsInstance(rol0.organization, Organization)
+        self.assertEqual(rol0.organization.name, 'Example')
+        self.assertEqual(rol0.start, MIN_PERIOD_DATE)
+        self.assertEqual(rol0.end, MAX_PERIOD_DATE)
+
+    def test_valid_organizations_stream(self):
+        """Check whether it parses organizations section from a valid stream"""
+
+        stream = self.read_file('data/stackalytics_valid.json')
+
+        parser = StackalyticsParser(stream)
+        orgs = parser.organizations
+        orgs.sort(key=lambda x: x.name)
+
+        # Check parsed organizations
+        self.assertEqual(len(orgs), 2)
+
+        # Bitergia entry
+        org = orgs[0]
+        self.assertIsInstance(org, Organization)
+        self.assertEqual(org.name, 'Bitergia')
+
+        doms = org.domains
+        self.assertEqual(len(org.domains), 2)
+
+        dom = doms[0]
+        self.assertIsInstance(dom, Domain)
+        self.assertEqual(dom.domain, 'bitergia.net')
+
+        dom = doms[1]
+        self.assertIsInstance(dom, Domain)
+        self.assertEqual(dom.domain, 'bitergia.com')
+
+        # Example entry
+        org = orgs[1]
+        self.assertIsInstance(org, Organization)
+        self.assertEqual(org.name, 'Example')
+        self.assertEqual(len(org.domains), 0)
+
+    def test_not_valid_stream(self):
+        """Check whether it prints an error when parsing invalid streams"""
+
+        with self.assertRaisesRegexp(InvalidFormatError,
+                                     STACKALYTICS_INVALID_JSON_FORMAT_ERROR):
+            s = self.read_file('data/stackalytics_invalid.json')
+            StackalyticsParser(s)
+
+        with self.assertRaisesRegexp(InvalidFormatError,
+                                     STACKALYTICS_IDS_MISSING_KEYS_ERROR):
+            s = self.read_file('data/stackalytics_ids_missing_keys.json')
+            StackalyticsParser(s)
+
+        with self.assertRaisesRegexp(InvalidFormatError,
+                                     STACKALYTICS_ORGS_MISSING_KEYS_ERROR):
+            s = self.read_file('data/stackalytics_orgs_missing_keys.json')
+            StackalyticsParser(s)
+
+    def test_empty_stream(self):
+        """Check whether it raises an exception when the stream is empty"""
+
+        with self.assertRaisesRegexp(InvalidFormatError,
+                                     STACKALYTICS_STREAM_INVALID_ERROR):
+            StackalyticsParser("")
+
+    def test_none_stream(self):
+        """Check whether it raises an exception when the stream is None"""
+
+        with self.assertRaisesRegexp(InvalidFormatError,
+                                     STACKALYTICS_STREAM_INVALID_ERROR):
+            StackalyticsParser(None)
+
+
+if __name__ == "__main__":
+    unittest.main(buffer=True, exit=False)
