02ada66abadfd6591fab34675635a98bbe539f21
Santiago Dueñas <sduenas@bitergia.com>
2019-01-02 19:12:29 +0100

    [errors] Add base core exceptions
    This patch adds the base core exception from all defined
    exceptions will inherit. It also adds 'AlreadyExistsError'
    class to handle errors when inserting instances that
    exists in the database.
    
    These exceptions will be managed and handled internally by
    the SortingHat service.

---
+++ sortinghat/core/errors.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2019 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+CODE_BASE_ERROR = 1
+CODE_ALREADY_EXISTS_ERROR = 2
+
+
+class BaseError(Exception):
+    """Base class error.
+
+    Derived classes can overwrite error message declaring
+    'message' property.
+    """
+    code = CODE_BASE_ERROR
+    message = "SortingHat unknown error"
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.msg = self.message % kwargs
+
+    def __str__(self):
+        return self.msg
+
+    def __int__(self):
+        return self.code
+
+
+class AlreadyExistsError(BaseError):
+    """Exception raised when an entity already exists in the registry"""
+
+    message = "%(entity)s '%(eid)s' already exists in the registry"
+    code = CODE_ALREADY_EXISTS_ERROR
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.entity = kwargs['entity']
+        self.eid = kwargs['eid']
