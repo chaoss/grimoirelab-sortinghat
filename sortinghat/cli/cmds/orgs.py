dc576fa6b3475f17c63fd896bbfa16c5c41db6ad
Santiago Dueñas <sduenas@bitergia.com>
2020-03-25 14:21:49 +0100

    [client] Add orgs command group
    This commit adds a group that will be the entry point
    to execute all commands related to organizations
    and domains.
    
    Signed-off-by: Santiago Dueñas <sduenas@bitergia.com>

---
+++ sortinghat/cli/cmds/orgs.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2020 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     Santiago Dueñas <sduenas@bitergia.com>
+#
+
+import click
+
+from ..utils import (sh_client_cmd_options,
+                     sh_client)
+
+
+@click.group()
+@sh_client_cmd_options
+@sh_client
+def orgs(ctx, user, password, host, port, server_path, disable_ssl):
+    """List, add or delete organizations and domains on the registry."""
+
+    pass
