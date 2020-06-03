e911beb5ec6023f516f00c5b43f31c2002130844
David Moreno <dmoreno@bitergia.com>
2020-06-03 18:14:26 +0200

    [ui] Add test for the getIndividuals query
    This commit adds a test of the expecting behavior of the
    getIndividuals query, matching the snapshot and checking that
    the query is called and the setData saved correctly the query
    response mocked.
    
    Signed-off-by: David Moreno <dmoreno@bitergia.com>

---
+++ ui/tests/unit/queries.spec.js
@@ -0,0 +1,49 @@
+import { shallowMount } from "@vue/test-utils";
+import Vue from "vue";
+import Vuetify from "vuetify";
+import App from "@/App";
+import { getIndividuals } from "@/apollo/queries";
+
+Vue.use(Vuetify);
+
+describe("App", () => {
+  test("mock query for getIndividuals", async () => {
+    const responseMocked = {
+      data: {
+        individuals: {
+          entities: [
+            {
+              mk: "172188fd88c1df2dd6d187b6f32cb6aced544aee",
+              identities: [{ name: "test name", __typename: "IdentityType" }],
+              profile: {
+                id: "7",
+                name: "test name",
+                __typename: "ProfileType"
+              },
+              __typename: "IndividualType"
+            }
+          ],
+          __typename: "IdentityPaginatedType"
+        }
+      }
+    };
+    const query = jest.fn(() => Promise.resolve(responseMocked));
+    const wrapper = shallowMount(App, {
+      Vue,
+      mocks: {
+        $apollo: {
+          query
+        }
+      }
+    });
+    let response = await getIndividuals(wrapper.vm.$apollo);
+    let individuals_mocked = response.data;
+    await wrapper.setData({
+      individuals_mocked
+    });
+
+    expect(query).toBeCalled();
+    expect(wrapper.element).toMatchSnapshot();
+    expect(wrapper.vm.individuals).toBe(responseMocked.data.individuals);
+  });
+});
