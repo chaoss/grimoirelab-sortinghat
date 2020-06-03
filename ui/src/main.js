ae1037869a6cea115d4f4911f2d7cd776c22a9fb
David Moreno <dmoreno@bitergia.com>
2020-06-03 16:27:06 +0200

    [ui] Add apollo library and configuration
    This commits adds all the needed steps to configure apollo in a
    vue application, including the installation version in the
    package.json file and the initialization of the library.
    
    Signed-off-by: David Moreno <dmoreno@bitergia.com>

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,11 +2,36 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueApollo from "vue-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: "http://localhost:8000/graphql/"
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
   vuetify,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
