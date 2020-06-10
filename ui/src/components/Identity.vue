6a61d74ef659eb0364ffa80cda39c44aca6ef711
Santiago Dueñas <sduenas@bitergia.com>
2020-06-10 19:06:36 +0200

    [ui] Add component to display identities
    The component Identity displays in a row the different
    attributes of an identity: uuid, name, email, username,
    and source.
    
    Take into account the source is an optional prop. When its
    value is 'null' it will not display anything.
    
    Signed-off-by: Santiago Dueñas <sduenas@bitergia.com>

---
+++ ui/src/components/Identity.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-row no-gutters>
+    <v-col cols="2">
+      <v-chip class="text-center" outlined tile>
+        {{ uuid }}
+      </v-chip>
+    </v-col>
+    <v-col class="ma-2 text-center">
+      <span>{{ name }}</span>
+    </v-col>
+    <v-col class="ma-2 text-center">
+      <span>{{ email }}</span>
+    </v-col>
+    <v-col class="ma-2 text-center">
+      <span>{{ username }}</span>
+    </v-col>
+    <v-col class="ma-2 text-center" v-if="source !== null">
+      <span>{{ source }}</span>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "identity",
+  props: {
+    uuid: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    source: {
+      type: String,
+      required: false,
+      default: null
+    }
+  }
+};
+</script>
