ace901d51762217e0ed42536dbe82d58357e4ff1
Eva Millán <evamillan@bitergia.com>
2020-09-22 10:53:21 +0200

    [ui] Pass list of sources to 'IndividualCard' from 'ProfileCard' and 'IndividualsGrid'
    Allows 'ProfileCard' and 'IndividualsGrid' to
    pass an array of data sources to the 'IndividualCard'
    component they embed.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

--- ui/src/components/IndividualsGrid.stories.js
+++ ui/src/components/IndividualsGrid.stories.js
@@ -20,49 +20,119 @@ export const Default = () => ({
           profile: {
             id: "1",
             name: "Tom Marvolo Riddle"
-          }
+          },
+          identities: [
+            {
+              source: "GitLab"
+            },
+          ]
         },
         {
           profile: {
             id: "2",
             name: "Harry Potter"
-          }
+          },
+          identities: [
+            {
+              source: "GitHub"
+            },
+            {
+              source: "git"
+            },
+            {
+              source: "Others"
+            }
+          ]
         },
         {
           profile: {
             id: "3",
             name: "Voldemort"
-          }
+          },
+          identities: [
+            {
+              source: "GitLab"
+            },
+          ]
         },
         {
           profile: {
             id: "4",
             name: "Hermione Granger"
-          }
+          },
+          identities: [
+            {
+              source: "GitHub"
+            },
+            {
+              source: "git"
+            },
+            {
+              source: "Others"
+            }
+          ]
         },
         {
           profile: {
             id: "5",
             name: "Ambus Dumbledore"
-          }
+          },
+          identities: [
+            {
+              source: "GitHub"
+            },
+            {
+              source: "git"
+            }
+          ]
         },
         {
           profile: {
             id: "6",
             name: "Ron Weasley"
-          }
+          },
+          identities: [
+            {
+              source: "GitHub"
+            },
+            {
+              source: "git"
+            },
+            {
+              source: "Others"
+            }
+          ]
         },
         {
           profile: {
             id: "7",
             name: "Severus Snape"
-          }
+          },
+          identities: [
+            {
+              source: "GitLab"
+            },
+            {
+              source: "GitHub"
+            },
+            {
+              source: "git"
+            }
+          ]
         },
         {
           profile: {
             id: "8",
             name: "Hagrid"
-          }
+          },
+          identities: [
+            {
+              source: "Git"
+            },
+            {
+              source: "Others"
+            }
+          ]
         }
       ]
     }
@@ -73,6 +143,9 @@ export const Empty = () => ({
   template: individualsGridTemplate,
   props: {
     individuals: {
+      default: () => []
+    },
+    sources: {
       default: () => []
     }
   }
