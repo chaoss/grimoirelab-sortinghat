433a2642e76c97f9a0e2aeb3520c2ffeb85d32e5
Eva Millán <evamillan@bitergia.com>
2020-10-29 15:56:16 +0100

    [ui] Implement 'ExpandedOrganization' component
    Adds the 'ExpandedOrganization' component, which
    displays a list of the organization's domains.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

---
+++ ui/src/components/ExpandedOrganization.stories.js
@@ -0,0 +1,30 @@
+import { storiesOf } from "@storybook/vue";
+
+import ExpandedOrganization from "./ExpandedOrganization.vue";
+
+export default {
+  title: "ExpandedOrganization",
+  excludeStories: /.*Data$/
+};
+
+
+const ExpandedOrganizationTemplate = `<expanded-organization :domains="domains" />`;
+
+export const Default = () => ({
+  components: { ExpandedOrganization },
+  template: ExpandedOrganizationTemplate,
+  data: () => ({
+    domains: [
+      { domain: "hogwarts.edu" },
+      { domain: "hogwarts.com" }
+    ]
+  })
+});
+
+export const Empty = () => ({
+  components: { ExpandedOrganization },
+  template: ExpandedOrganizationTemplate,
+  data: () => ({
+    domains: []
+  })
+});
---
+++ ui/src/components/ExpandedOrganization.vue
@@ -0,0 +1,24 @@
+<template>
+  <td>
+    <v-list dense>
+      <v-subheader>Domains ({{ domains.length }})</v-subheader>
+      <v-list-item v-for="(item, index) in domains" :key="index">
+        <v-list-item-content>
+          <v-list-item-title>{{ item.domain }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </td>
+</template>
+
+<script>
+export default {
+  name: "ExpandedOrganization",
+  props: {
+    domains: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
