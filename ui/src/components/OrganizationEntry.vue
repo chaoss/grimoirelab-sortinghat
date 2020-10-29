dc8117bd358a0b34aad75a204fbb419608fba3e2
Eva Millán <evamillan@bitergia.com>
2020-10-29 15:58:52 +0100

    [ui] Add expanded information to OrganizationsTable
    Each entry on OrganizationsTable can be expanded in
    order to display a list of its domains.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

--- ui/src/components/OrganizationEntry.vue
+++ ui/src/components/OrganizationEntry.vue
@@ -2,6 +2,13 @@
   <tr>
     <td class="text--body-1">{{ name }}</td>
     <td class="text-right text--secondary">{{ enrollments }}</td>
+    <td class="text-right" width="40">
+      <v-btn icon @click.stop="$emit('expand')">
+        <v-icon>
+          {{ isExpanded ? "mdi-chevron-up" : "mdi-chevron-down" }}
+        </v-icon>
+      </v-btn>
+    </td>
   </tr>
 </template>
 
@@ -15,6 +22,10 @@ export default {
     },
     enrollments: {
       type: Number,
+      required: true
+    },
+    isExpanded: {
+      type: Boolean,
       required: true
     }
   }
