8517fb2897f864994a7304806eae5709ca422fa8
Eva Millán <evamillan@bitergia.com>
2020-11-18 11:46:34 +0100

    [ui] Add Login component
    Implements a login component, which displays a
    form to enter and submit user credentials. The
    component uses the login store action on submit.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

---
+++ ui/src/components/Login.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-content class="align-center">
+    <v-card class="mx-auto pa-7" max-width="500">
+      <v-card-title class="display-1 mb-2">Welcome</v-card-title>
+      <v-card-subtitle class="mb-3">Please log in to continue</v-card-subtitle>
+      <v-card-text>
+        <v-form ref="form">
+          <v-text-field v-model="username" label="Username" filled />
+          <v-text-field
+            v-model="password"
+            label="Password"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showPassword ? 'text' : 'password'"
+            filled
+            @click:append="showPassword = !showPassword"
+          />
+          <v-alert v-if="errorMessage" text type="error">
+            {{ errorMessage }}
+          </v-alert>
+          <v-btn
+            depressed
+            block
+            color="primary"
+            :disabled="disableSubmit"
+            @click.prevent="submit"
+          >
+            Log in
+          </v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-content>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "Login",
+  data: () => ({
+    username: "",
+    password: "",
+    showPassword: false,
+    errorMessage: ""
+  }),
+  computed: {
+    disableSubmit() {
+      return this.username.length < 3 || this.password.length < 3;
+    }
+  },
+  methods: {
+    ...mapActions(["login"]),
+    async submit() {
+      try {
+        const authDetails = {
+          apollo: this.$apollo,
+          username: this.username,
+          password: this.password
+        };
+        const response = await this.login(authDetails);
+        if (response) {
+          this.$router.push("/");
+        }
+      } catch (error) {
+        this.errorMessage = error;
+      }
+    }
+  }
+};
+</script>
