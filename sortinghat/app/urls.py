a93a4452bd975dc8ee980eea869a5c10403dcf18
Eva Millán <evamillan@bitergia.com>
2021-09-23 16:03:00 +0200

    [urls] Handle Vue routes from Django
    Uses Django's 're_path' to capture Vue router's dynamic paths.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

--- sortinghat/app/urls.py
+++ sortinghat/app/urls.py
@@ -2,7 +2,7 @@
 
 
 from django.conf import settings
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import TemplateView
 
 from sortinghat.core.views import SortingHatGraphQLView
@@ -10,6 +10,6 @@ from sortinghat.core.views import SortingHatGraphQLView
 from .schema import schema
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="index.html")),
-    path('api/', SortingHatGraphQLView.as_view(graphiql=settings.DEBUG, schema=schema))
+    path('api/', SortingHatGraphQLView.as_view(graphiql=settings.DEBUG, schema=schema)),
+    re_path(r'^(?!static).*$', TemplateView.as_view(template_name="index.html"))
 ]
