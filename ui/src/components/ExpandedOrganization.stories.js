2e955408bb89344ab2ee3146cb671eb290dfbaf7
Eva Millán <evamillan@bitergia.com>
2021-12-30 16:29:10 +0100

    [ui] Display unaffiliated teams on ExpandedOrganization
    Adds the 'isGroup' props to request the expanded item's teams accordingly.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

--- ui/src/components/ExpandedOrganization.stories.js
+++ ui/src/components/ExpandedOrganization.stories.js
@@ -1,68 +1,83 @@
-import ExpandedOrganization from './ExpandedOrganization.vue';
+import ExpandedOrganization from "./ExpandedOrganization.vue";
 
 export default {
-  title: 'ExpandedOrganization',
-  excludeStories: /.*Data$/,
+  title: "ExpandedOrganization",
+  excludeStories: /.*Data$/
 };
 
-const ExpandedOrganizationTemplate = `<expanded-organization :domains="domains" :organization="organization" :add-team="addTeam" :delete-team="deleteTeam" :fetch-teams="fetchTeams"/>`;
+const ExpandedOrganizationTemplate = `
+  <expanded-organization
+    :domains="domains"
+    :organization="organization"
+    :is-group="isGroup"
+    :add-team="addTeam"
+    :delete-team="deleteTeam"
+    :fetch-teams="fetchTeams"
+  />`;
 
 export const Default = () => ({
   components: { ExpandedOrganization },
   template: ExpandedOrganizationTemplate,
   data: () => ({
-    domains: [{ domain: 'hogwarts.edu' }, { domain: 'hogwarts.com' }],
-    organization: 'Hogwarts',
+    isGroup: false,
+    domains: [{ domain: "ministryofmagic.uk" }, { domain: "mom.com" }],
+    organization: "Ministry of Magic",
     query: [
       {
         data: {
           teams: {
             entities: [
               {
-                name: 'BU1',
-                numchild: 2,
+                name: "Committee on Experimental Charms",
+                numchild: 0
               },
               {
-                name: 'BU2',
-                numchild: 0,
+                name: "Department of Magical Law Enforcement",
+                numchild: 2
               },
               {
-                name: 'BU3',
-                numchild: 1,
+                name: "Department of Magical Accidents and Catastrophes",
+                numchild: 0
               },
               {
-                name: 'Team1',
-                parent: 'BU1',
-                numchild: 0,
+                name:
+                  "Department for the Regulation and Control of Magical Creatures",
+                numchild: 1
               },
               {
-                name: 'Team2',
-                parent: 'BU1',
-                numchild: 0,
+                name: "Auror Office",
+                parent: "Department of Magical Law Enforcement",
+                numchild: 0
               },
               {
-                name: 'Team4',
-                parent: 'BU3',
-                numchild: 0,
+                name: "Improper Use of Magic Office",
+                parent: "Department of Magical Law Enforcement",
+                numchild: 0
               },
-            ],
-          },
-        },
-      },
-    ],
+              {
+                name: "Beast Division",
+                parent:
+                  "Department for the Regulation and Control of Magical Creatures",
+                numchild: 0
+              }
+            ]
+          }
+        }
+      }
+    ]
   }),
   methods: {
     fetchTeams(filters) {
       let data = [];
-      if (Object.keys(filters).includes('parent')) {
-        this.query[0].data.teams.entities.forEach((team) => {
-          if (team['parent'] === filters['parent']) {
+      if (Object.keys(filters).includes("parent")) {
+        this.query[0].data.teams.entities.forEach(team => {
+          if (team["parent"] === filters["parent"]) {
             data.push(team);
           }
         });
       } else {
-        this.query[0].data.teams.entities.forEach((team) => {
-          if (team['parent'] === undefined) {
+        this.query[0].data.teams.entities.forEach(team => {
+          if (team["parent"] === undefined) {
             data.push(team);
           }
         });
@@ -71,58 +86,92 @@ export const Default = () => ({
         data: {
           teams: {
             entities: data
-          },
-        },
+          }
+        }
       };
       return resp;
     },
     addTeam(team, organization, parent) {
       const insertData = {
-        name: team,
+        name: team
       };
       if (parent) {
-        insertData['parent'] = parent;
+        insertData["parent"] = parent;
       }
       this.query[0].data.teams.entities.push(insertData);
       return true;
     },
     deleteTeam(team, organization) {
       this.query[0].data.teams.entities = this.query[0].data.teams.entities.filter(
-        (elem) => elem.name != team,
+        elem => elem.name != team
       );
       return true;
+    }
+  }
+});
+
+export const Group = () => ({
+  components: { ExpandedOrganization },
+  template: ExpandedOrganizationTemplate,
+  data: () => ({
+    isGroup: true,
+    organization: "Witch Weekly",
+    domains: null,
+    query: {
+      data: {
+        teams: {
+          entities: [
+            {
+              name: "Witches' League",
+              numchild: 0
+            }
+          ]
+        }
+      }
+    }
+  }),
+  methods: {
+    fetchTeams() {
+      return this.query;
     },
-  },
+    addTeam() {
+      return;
+    },
+    deleteTeam() {
+      return;
+    }
+  }
 });
 
 export const Empty = () => ({
   components: { ExpandedOrganization },
   template: ExpandedOrganizationTemplate,
   data: () => ({
+    isGroup: false,
     domains: [],
-    organization: 'Hogwarts',
+    organization: "Hogwarts",
     query: [
       {
         data: {
           teams: {
-            entities: [],
-          },
-        },
-      },
-    ],
+            entities: []
+          }
+        }
+      }
+    ]
   }),
   methods: {
     fetchTeams(filters) {
       let data = [];
-      if (Object.keys(filters).includes('parent')) {
-        this.query[0].data.teams.entities.forEach((team) => {
-          if (team['parent'] === filters['parent']) {
+      if (Object.keys(filters).includes("parent")) {
+        this.query[0].data.teams.entities.forEach(team => {
+          if (team["parent"] === filters["parent"]) {
             data.push(team);
           }
         });
       } else {
-        this.query[0].data.teams.entities.forEach((team) => {
-          if (team['parent'] === undefined) {
+        this.query[0].data.teams.entities.forEach(team => {
+          if (team["parent"] === undefined) {
             data.push(team);
           }
         });
@@ -130,28 +179,28 @@ export const Empty = () => ({
       const resp = {
         data: {
           teams: {
-            entities: data,
-          },
-        },
+            entities: data
+          }
+        }
       };
 
       return resp;
     },
     addTeam(team, organization, parent) {
       const insertData = {
-        name: team,
+        name: team
       };
       if (parent) {
-        insertData['parent'] = parent;
+        insertData["parent"] = parent;
       }
       this.query[0].data.teams.entities.push(insertData);
       return true;
     },
     deleteTeam(team, organization) {
       this.query[0].data.teams.entities = this.query[0].data.teams.entities.filter(
-        (elem) => elem.name != team,
+        elem => elem.name != team
       );
       return true;
-    },
-  },
+    }
+  }
 });
