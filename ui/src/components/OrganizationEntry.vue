ff23d0120b7cf7a460557134e0cfd546f2837102
Eva Millán <evamillan@bitergia.com>
2023-04-17 15:40:02 +0200

    [ui] Show Organizations table actions in menu
    Replaces the buttons to edit and delete an organization
    with an action menu and adds the option to create a team.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

--- ui/src/components/OrganizationEntry.vue
+++ ui/src/components/OrganizationEntry.vue
@@ -6,7 +6,9 @@
     @dragenter.prevent="isDropZone($event, true)"
     @dragleave.prevent="isDropZone($event, false)"
   >
-    <td class="font-weight-medium">{{ name }}</td>
+    <td class="font-weight-medium pl-8">
+      {{ name }}
+    </td>
     <td class="text-right text--secondary">
       <v-tooltip bottom transition="expand-y-transition" open-delay="200">
         <template v-slot:activator="{ on }">
@@ -23,13 +25,43 @@
         <span>Enrollments</span>
       </v-tooltip>
     </td>
-    <td class="text-right" width="140">
-      <v-btn v-if="isEditable" icon @click.stop="$emit('edit')">
-        <v-icon small> mdi-lead-pencil </v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="$emit('delete')">
-        <v-icon small> mdi-delete </v-icon>
-      </v-btn>
+    <td class="text-right">
+      <v-menu v-model="showMenu" left nudge-left="16">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon small v-bind="attrs" v-on="on">
+            <v-icon small>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list dense>
+          <v-list-item @click="$emit('edit')">
+            <v-list-item-title> View/edit domains </v-list-item-title>
+          </v-list-item>
+          <v-edit-dialog
+            large
+            @save="
+              $emit('addTeam', newTeam);
+              newTeam = '';
+            "
+            @cancel="newTeam = ''"
+          >
+            <v-list-item @click="showMenu = false">
+              <v-list-item-title>Add a team</v-list-item-title>
+            </v-list-item>
+            <template v-slot:input>
+              <h6 class="text-subtitle-2 mt-2">Add team to {{ name }}</h6>
+              <v-text-field
+                v-model="newTeam"
+                label="Name"
+                maxlength="50"
+                single-line
+              />
+            </template>
+          </v-edit-dialog>
+          <v-list-item @click="$emit('delete')">
+            <v-list-item-title>Delete organization</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
       <v-btn icon @click.stop="$emit('expand')">
         <v-icon>
           {{ isExpanded ? "mdi-chevron-up" : "mdi-chevron-down" }}
@@ -64,14 +96,12 @@ export default {
   data() {
     return {
       dropZone: false,
+      showMenu: false,
+      newTeam: "",
     };
   },
   methods: {
     onDrop(event) {
-      // TODO: Remove when individuals can be affiliated to teams
-      if (!this.isEditable) {
-        return;
-      }
       this.dropZone = false;
       const type = event.dataTransfer.getData("type");
       if (type === "enrollFromOrganization") {
@@ -83,15 +113,11 @@ export default {
       if (droppedIndividuals.length > 0) {
         this.$emit("enroll", {
           uuids: droppedIndividuals.map((individual) => individual.uuid),
-          organization: this.name,
+          group: this.name,
         });
       }
     },
     isDropZone(event, isDragging) {
-      // TODO: Remove when individuals can be affiliated to teams
-      if (!this.isEditable) {
-        return;
-      }
       const type = event.dataTransfer.getData("type");
       // Can't use 'getData' while dragging on Chrome
       const types = event.dataTransfer.types;
@@ -116,7 +142,30 @@ export default {
 tr {
   cursor: pointer;
 }
+
 td:last-of-type {
-  min-width: 105px;
+  width: 100px;
+}
+
+tr[draggable="true"] {
+  td:first-of-type {
+    position: relative;
+
+    &::before {
+      font: normal normal normal 24px/1 "Material Design Icons";
+      font-size: 1rem;
+      content: "\F01DD";
+      color: rgba(0, 0, 0, 0.62);
+      position: absolute;
+      left: 4px;
+      top: 30%;
+      opacity: 0;
+      cursor: grab;
+    }
+  }
+
+  &:hover td::before {
+    opacity: 1;
+  }
 }
 </style>
