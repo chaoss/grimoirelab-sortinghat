6c770f3db6da996a2b5fe13cbf4f6cdd268e5532
Eva Millán <evamillan@bitergia.com>
2023-05-17 13:19:31 +0200

    [ui] Add MatchesModal component
    Creates a component to generate matching recommendations
    for a given individual.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

---
+++ ui/src/components/MatchesModal.stories.js
@@ -0,0 +1,51 @@
+import MatchesModal from "./MatchesModal.vue";
+
+export default {
+  title: "MatchesModal",
+  excludeStories: /.*Data$/,
+};
+
+const template = `
+  <div data-app="true" class="ma-auto">
+    <v-btn color="primary" dark @click.stop="isOpen = true">
+      Open modal
+    </v-btn>
+    <matches-modal
+      :is-open.sync="isOpen"
+      :recommend-matches="recommendMatches"
+      uuid="abc123"
+    />
+  </div>
+`;
+
+export const Default = () => ({
+  components: { MatchesModal },
+  template: template,
+  data: () => ({
+    isOpen: false,
+  }),
+  methods: {
+    recommendMatches() {
+      return {
+        data: {
+          recommendMatches: {
+            jobId: "b65d2170-a560-4b20-954e-fc8c9f5afdd4"
+          }
+        }
+      }
+    }
+  },
+});
+
+export const Error = () => ({
+  components: { MatchesModal },
+  template: template,
+  data: () => ({
+    isOpen: false,
+  }),
+  methods: {
+    recommendMatches() {
+      throw new TypeError("Error creating job")
+    }
+  },
+});
