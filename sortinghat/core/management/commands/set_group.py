ec60168e9af87f56bac9a256a904354ff18ef28d
Eva Millán <evamillan@bitergia.com>
2024-07-04 17:09:18 +0200

    [cmd] Assign users to groups using commands
    Allows users to be assigned to permission groups using the
    following command:
    ```
    sortinghat-admin set-user-group username group
    ```
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

---
+++ sortinghat/core/management/commands/set_group.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     Eva Millán <evamillan@bitergia.com>
+#
+
+from django.core.management import BaseCommand, CommandError
+from django.core import exceptions
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
+from django.db import DEFAULT_DB_ALIAS
+
+
+class Command(BaseCommand):
+    help = "Assign a user to a permission group"
+
+    def add_arguments(self, parser):
+        parser.add_argument('username')
+        parser.add_argument('group_name')
+        parser.add_argument(
+            '--database',
+            default=DEFAULT_DB_ALIAS,
+            help='Specifies the database to use. Default is "default".',
+        )
+
+    def handle(self, *args, **options):
+        try:
+            group = Group.objects.using(options['database']).get(name=options['group_name'])
+            user = get_user_model().objects.get(username=options['username'])
+            user.groups.set([group.id])
+        except Group.DoesNotExist:
+            raise CommandError(f"Group '{options['group_name']}' not found")
+        except exceptions.ObjectDoesNotExist:
+            raise CommandError(f"User '{options['username']}' not found")
