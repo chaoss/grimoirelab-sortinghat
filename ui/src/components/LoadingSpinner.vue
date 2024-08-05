b83dc3c09e9389957d79bce54260a98268df62ed
Eva Millán <evamillan@bitergia.com>
2024-08-05 12:58:13 +0200

    [ui] Add LoadingSpinner component
    Adds a component to display a loading spinner and an optional
    loading message.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

---
+++ ui/src/components/LoadingSpinner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="d-flex flex-column justify-center align-center pa-4 h-100">
+    <v-progress-circular :size="size" color="primary" indeterminate />
+    <p v-if="label" class="subtitle-1 text-medium-emphasis mt-4">{{ label }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "LoadingSpinner",
+  props: {
+    label: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    size: {
+      type: [String, Number],
+      required: false,
+      default: 48,
+    },
+  },
+};
+</script>
