c160050a18fd85c245de8270f6dc62c8c7154d91
Eva Millán <evamillan@bitergia.com>
2025-09-08 17:37:35 +0200

    [schema] Order merge recommendations by number of suggestions
    Orders the list of merge recommendations by the individual's
    number of suggestions.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

--- sortinghat/core/schema.py
+++ sortinghat/core/schema.py
@@ -2129,7 +2129,15 @@ class SortingHatQuery:
         else:
             query = MergeRecommendation.objects.filter(applied=None)
 
-        query = query.order_by('created_at')
+        query = query.annotate(individual_matches_count=Count(
+            'individual1__match_recommendation_individual_1',
+            filter=Q(individual1__match_recommendation_individual_1__applied=None),
+            distinct=True
+        ) + Count(
+            'individual1__match_recommendation_individual_2',
+            filter=Q(individual1__match_recommendation_individual_2__applied=None),
+            distinct=True
+        ))
 
         return RecommendedMergePaginatedType.create_paginated_result(query,
                                                                      page,
